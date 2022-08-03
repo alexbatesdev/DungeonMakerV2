622b728e4e789a434e33254b5cc7a51975e1fb24
Alex <mcbuzzer@gmail.com>
2022-08-03 11:17:57 -0600

    Added comment in main

--- src/edu/neumont/Main.java
+++ src/edu/neumont/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Model model = new Model();
         model.run();
+        // Wow a new change!
+        // I don't want to break my old code lol
+        // Added this for an assignment about git
     }
 }
